9a13e034b43cfe0ee922e16d91dcb3c4abf2a90a
arielfrozza <arielfrozza@gmail.com>
2018-06-06 22:34:26 -0300

    Configered static IP for eth1.

--- Ubuntu-16.04-Newton/scripts/init.sh
+++ Ubuntu-16.04-Newton/scripts/init.sh
@@ -11,3 +11,10 @@ apt-get update
 apt-get -y upgrade
 apt-get -y install git vim
 
+echo '' >> /etc/network/interfaces
+echo '#The contents below were added by Packer. Do not modify.' >> /etc/network/interfaces
+echo 'auto eth1' >> /etc/network/interfaces
+echo 'iface eth1 inet static' >> /etc/network/interfaces
+echo '      address 192.168.56.111' >> /etc/network/interfaces
+echo '      netmask 255.255.255.0' >> /etc/network/interfaces
+echo '      nameservers 8.8.8.8' >> /etc/network/interfaces
